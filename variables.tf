33714c85890a1d0d62aa2e04f6c1fdb6b5903104
Yaraslau Zhukouski <yaroslav.zhukovski@gmail.com>
2025-07-13 20:18:36 +0200

    New variables

--- variables.tf
+++ variables.tf
@@ -1,18 +1,3 @@
-variable "client_id" {
-  description = "The client ID for the Azure service principal."
-  type        = string
-
-}
-
-variable "tenant_id" {
-  description = "The tenant ID for the Azure service principal."
-  type        = string
-}
-
-variable "subscription_id" {
-  description = "The subscription ID for the Azure resources."
-  type        = string
-}
 
 variable "location" {
   description = "The Azure region where resources will be created."
