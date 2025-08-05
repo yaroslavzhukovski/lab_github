dc215792cb29d7e6349843c06be2edcd5aec9f58
Yaraslau Zhukouski <yaroslav.zhukovski@gmail.com>
2025-08-05 14:43:30 +0200

    Policy blob

--- modules/policy_assignment/main.tf
+++ modules/policy_assignment/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group_policy_assignment" "this" {
   name                 = var.name
   display_name         = var.display_name
   policy_definition_id = var.policy_definition_id
-  resource_group_id    = var.scope # Scope is now the RG ID
+  resource_group_id    = var.scope
 
-  parameters = var.parameters
+  parameters = jsonencode(var.parameters)
 }
