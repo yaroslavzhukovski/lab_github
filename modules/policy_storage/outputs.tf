9063c1b07f0e5e5c1b20c214b4a1dde0fd0923b8
Yaraslau Zhukouski <yaroslav.zhukovski@gmail.com>
2025-08-05 13:43:14 +0200

    Policy blob

--- modules/policy_storage/outputs.tf
+++ modules/policy_storage/outputs.tf
@@ -1,14 +1,7 @@
-output "https_only_policy_id" {
-  value = azurerm_policy_definition.https_only.id
-}
-
-output "public_access_deny_policy_id" {
-  value = azurerm_policy_definition.public_access_deny.id
-}
 output "https_only_assignment_id" {
-  value = azurerm_policy_assignment.https_only_assign.id
+  value = azurerm_policy_assignment.storage_https.id
 }
 
 output "public_access_deny_assignment_id" {
-  value = azurerm_policy_assignment.public_access_deny_assign.id
+  value = azurerm_policy_assignment.storage_no_public_blob.id
 }
